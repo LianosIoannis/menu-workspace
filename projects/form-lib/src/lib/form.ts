00716dabb0f7607a9f71486c9680aed4af2f29f3
jlianos <J.lianos@futuresoft.gr>
2026-06-10 15:25:02 +0300

    added form-lib to workspace

---
+++ projects/form-lib/src/lib/form.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "lib-form-lib",
+	imports: [],
+	templateUrl: "./form.html",
+	styleUrl: "./form.css",
+})
+export class FormLib {}
---
+++ projects/form-lib/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of form-lib
+ */
+
+export * from './lib/form';
